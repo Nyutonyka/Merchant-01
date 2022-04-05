344ce82505ecdc571e9c39099af02674a3c73fe6
Babich <nyutonyka@gmail.com>
2022-04-05 14:36:09 +0300

    Add product information

--- src/app/App.java
+++ src/app/App.java
@@ -2,7 +2,26 @@ package app;
 
 public class App {
 
+    static String productName;
+    static int quantity;
+    static double price;
+    static ProductA product;
+    static String infoProduct;
+
     public static void main(String[] args) {
-        System.out.println("New program");
+        initVars();
+        processData();
+    }
+
+    private static void initVars() {
+        productName = "апельсины";
+        quantity = 1000;
+        price = 19.99;
+    }
+
+    private static void processData() {
+        product = new ProductA(productName, quantity, price);
+        infoProduct = product.infoProduct();
+        double sales = product.calcSales(quantity, price);
     }
 }
