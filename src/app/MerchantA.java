5f5e2fd1c007e1dd5f019efc957dad84eccd65c9
Babich <nyutonyka@gmail.com>
2022-04-05 14:56:00 +0300

    Add bonus calculation

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -1,11 +1,24 @@
 package app;
 
-public class MerchantA extends Merchant{
+public class MerchantA extends Merchant implements Bonus{
 
     String email;
 
     public MerchantA(String name, String phone, String email) {
         super(name, phone);
         this.email = email;
+    }
+
+    @Override
+    public double calcBonus(double sales) {
+        double bonus;
+        if (sales <= 10000) {
+            bonus = sales * 5 / 100;
+        } else if (sales > 10000 && sales <= 30000) {
+            bonus = sales * 10 / 100;
+        } else {
+            bonus = sales * 15 / 100;
+        }
+        return bonus;
     }
 }
